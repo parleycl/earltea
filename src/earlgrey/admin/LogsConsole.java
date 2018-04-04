5282384d5382c63feb5c6267dcdc148dac93de64
Angelo Calvo A <angelo.calvoa@gmail.com>
2018-04-04 17:41:36 -0300

    Modify the console service to work with new notation

--- src/earlgrey/admin/LogsConsole.java
+++ src/earlgrey/admin/LogsConsole.java
@@ -7,9 +7,11 @@ import java.util.Arrays;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import earlgrey.annotations.CORS;
 import earlgrey.annotations.Console;
 import earlgrey.annotations.Controller;
 import earlgrey.annotations.ControllerAction;
+import earlgrey.annotations.GET;
 import earlgrey.annotations.ParamRequire;
 import earlgrey.annotations.Policie;
 import earlgrey.annotations.Route;
@@ -22,10 +24,12 @@ import earlgrey.core.Properties;
 
 @Console(description = "Controlador para gestionar los logs", name = "Logs", version = 1)
 @Route(route = "/logs")
+@CORS
 public class LogsConsole extends ControllerCore{
 	//CONTROLADOR PARA GESTIONAR LAS PROPERTIES
 	@ControllerAction(description = "Metodo para obtener el historial de logs.", name = "logs_history", version = 1)
 	@Route(route = "/gethistory")
+	@GET
 	public static void GetHistory(HttpRequest req, HttpResponse res){
 		File[] logs = Logging.getHistory();
 		JSONArray historial = new JSONArray();
@@ -39,6 +43,7 @@ public class LogsConsole extends ControllerCore{
 	@Route(route = "/getlog")
 	@ParamRequire(name = "from", type = double.class)
 	@ParamRequire(name = "file", type = String.class)
+	@GET
 	public static void getFile(HttpRequest req, HttpResponse res){
 		Logging log = new Logging("Logs");
 		JSONArray lineas = Logging.getFile(req.getParam("file"), Integer.valueOf(req.getParam("from")));
--- src/earlgrey/admin/PropertiesManager.java
+++ src/earlgrey/admin/PropertiesManager.java
@@ -6,9 +6,13 @@ import java.util.Arrays;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import earlgrey.annotations.CORS;
 import earlgrey.annotations.Console;
 import earlgrey.annotations.Controller;
 import earlgrey.annotations.ControllerAction;
+import earlgrey.annotations.GET;
+import earlgrey.annotations.PATCH;
+import earlgrey.annotations.POST;
 import earlgrey.annotations.ParamRequire;
 import earlgrey.annotations.Policie;
 import earlgrey.annotations.Route;
@@ -22,10 +26,12 @@ import earlgrey.core.Properties;
 
 @Console(description = "Controlador para gestionar las properties", name = "Properties", version = 1)
 @Route(route = "/properties")
+@CORS
 public class PropertiesManager extends ControllerCore{
 	//CONTROLADOR PARA GESTIONAR LAS PROPERTIES
 	@ControllerAction(description = "Metodo para obtener los entornos de properties.", name = "get_env", version = 1)
 	@Route(route = "/getenv")
+	@GET
 	public static void GetEnviroments(HttpRequest req, HttpResponse res){
 		Properties prop = Properties.getInstance();
 		JSONObject obj = prop.getPropertiesEnv();
@@ -39,6 +45,7 @@ public class PropertiesManager extends ControllerCore{
 	@ControllerAction(description = "Metodo para designar un entorno de properties por defecto.", name = "set_env", version = 1)
 	@ParamRequire(name = "env", type = String.class)
 	@Route(route = "/setenv")
+	@POST
 	public static void SetEnviroments(HttpRequest req, HttpResponse res){
 		Properties prop = Properties.getInstance();
 		String env = req.getParam("env");
@@ -51,6 +58,7 @@ public class PropertiesManager extends ControllerCore{
 	@ControllerAction(description = "Metodo para crear un entorno properties por defecto.", name = "create_env", version = 1)
 	@Route(route = "/createenv")
 	@ParamRequire(name = "name", type = String.class)
+	@POST
 	public static void createEnviroments(HttpRequest req, HttpResponse res){
 		String env = req.getParam("name");
 		JSONObject objeto = Properties.getInstance().createEnv(env);
@@ -61,6 +69,7 @@ public class PropertiesManager extends ControllerCore{
 	@Route(route = "/modprop")
 	@ParamRequire(name = "name", type = String.class)
 	@ParamRequire(name = "prop", type = String.class)
+	@PATCH
 	public static void modPropertie(HttpRequest req, HttpResponse res){
 		JSONObject obj = new JSONObject(req.getParam("prop"));
 		Properties.getInstance().setProp(obj,req.getParam("name"));
@@ -71,6 +80,7 @@ public class PropertiesManager extends ControllerCore{
 	}
 	@ControllerAction(description = "Metodo para verificar el estado de restarting.", name = "get_restart_status", version = 1)
 	@Route(route = "/getrestart")
+	@GET
 	public static void getRestartStatus(HttpRequest req, HttpResponse res){
 		JSONObject retorno = new JSONObject();
 		retorno.put("STATUS", Engine.getInstance().getRestartStatus());
@@ -79,6 +89,7 @@ public class PropertiesManager extends ControllerCore{
 	}
 	@ControllerAction(description = "Metodo para reiniciar el entorno de properties.", name = "restart_env", version = 1)
 	@Route(route = "/restart")
+	@POST
 	public static void restartEnv(HttpRequest req, HttpResponse res){
 		Properties.getInstance().restartProperties();
 		JSONObject retorno = new JSONObject();
