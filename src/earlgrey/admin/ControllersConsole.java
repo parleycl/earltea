f63656e19d680ce5a87d031873c22437f8c5519d
Angelo Calvo A <angelo.calvoa@gmail.com>
2018-05-29 18:58:44 -0400

    Add the controller console endpoint

--- src/earlgrey/admin/ControllersConsole.java
+++ src/earlgrey/admin/ControllersConsole.java
@@ -35,5 +35,41 @@ import earlgrey.def.SessionDef;
 @Route(route = "/controllers")
 @CORS
 public class ControllersConsole extends ControllerCore{
-	
+	@ControllerAction(description = "Method use to get the controllers", name = "Get Controllers", version = 1)
+	@GET
+	public static void getControllers(HttpRequest req, HttpResponse res){
+		JSONArray retorno = new JSONArray();
+		ResourceMaping resources = ResourceMaping.getInstance();
+		Hashtable<String, Class<?>> controllers = resources.getControllerTable();
+		Enumeration<String> keys = controllers.keys();
+		while(keys.hasMoreElements()) {
+			String key = keys.nextElement();
+			// route name
+			JSONObject controller = new JSONObject();
+			Class<?> ctllr = controllers.get(key);
+			Controller ctr = ctllr.getAnnotation(Controller.class);
+			controller.put("name", ctr.name());
+			controller.put("description", ctr.description());
+			controller.put("version", ctr.version());
+			controller.put("class_name", ctllr.getName());
+			// actions
+			JSONArray actions = new JSONArray();
+			Method[] methods = ctllr.getMethods();
+			for(int i=0; i<methods.length; i++) {
+				ControllerAction ctr_action = methods[i].getAnnotation(ControllerAction.class); 
+				if(ctr_action != null) {
+					JSONObject action_obj = new JSONObject();
+					action_obj.put("name", ctr_action.name());
+					action_obj.put("description", ctr_action.description());
+					action_obj.put("version", ctr_action.version());
+					action_obj.put("method_name", methods[i].getName());
+					actions.put(action_obj);
+				}
+			}
+			controller.put("actions", actions);
+			retorno.put(controller);
+		}
+		res.json(retorno);
+		return;
+	}
 }
