6c37f09d034b95287cda07a80127ddb8d6d8056e
Angelo Calvo A <angelo.calvoa@gmail.com>
2018-05-30 01:01:56 -0400

    Add users endpoint

---
+++ console/src/client/app/features/users/users.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+/**
+* This class represents the main application component.
+*/
+@Component({
+    moduleId: module.id,
+    selector: 'sd-users',
+    templateUrl: 'users.component.html',
+    styleUrls: ['users.component.css'],
+})
+export class UsersComponent {
+    constructor() {}
+}
